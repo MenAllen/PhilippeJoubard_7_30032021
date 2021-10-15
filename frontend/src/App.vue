a7ce9ff4dca467a40f024a2915abeb10c1749ed4
Philippe Joubard <philippe.joubard@sfr.fr>
2021-10-15 17:44:58 +0200

    minor fixes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,25 +18,29 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #3f3d56;
-
-	background: url("./assets/photo2.jpg") repeat center center;
+body { 
+	display: block;
+	background: url("./assets/photo2.jpg") no-repeat center center;
+	min-height: 100vh;
 	-webkit-background-size: cover;
 	-moz-background-size: cover;
 	background-size: cover;
 	-o-background-size: cover;
 }
 
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #3f3d56;
+}
+
 #message {
 	position: sticky;
 	z-index: 100;
-	top: 600px;
 	color: white;
+	top: 600px;
 	margin-left: 0;
 }
 </style>
